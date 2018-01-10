18d88b1831e8d25170bf253b32022626d64c5406
Alex-Farries <adfarries@gmail.com>
2018-01-11 00:20:31 +1100

    Add express server

---
+++ api/server.js
@@ -0,0 +1,30 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const server = express()
+
+// Middleware Plugins
+server.use(bodyParser.json())
+server.use(cors())
+
+server.use((error, req, res, next) => {
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
+
+server.listen(8000, (error) => {
+  if (error) {
+    console.error('Error starting', error)
+  }
+  else {
+    console.log('Server started at http://localhost:8000/')
+  }
+})
