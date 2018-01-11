9b002ee831968c67f7c7ec6bff6b181437fe725b
Alex-Farries <adfarries@gmail.com>
2018-01-12 10:20:40 +1100

    Add api folder to web * Swap editorState for contentState, hopefully will speed things up

--- web/src/App.js
+++ web/src/App.js
@@ -1,35 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Editor } from 'react-draft-wysiwyg'
-import { EditorState } from 'draft-js'
+import { convertFromRaw } from 'draft-js'
 import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+
+// Output from sending email:
+
+// {
+//   subject: 'some string';
+//   created_at: new Date(); is this necessary with mongodb / embedded timestamp?
+//   content: contentState
+// }
 
 class App extends Component {
   state = {
-    editorState: EditorState.createEmpty()
+    contentState: null
   }
 
-  onEditorStateChange = (editorState) => {
-    this.setState({ editorState })
+  onContentStateChange = (contentState) => {
+    this.setState({ contentState })
   }
 
   render() {
-    const { editorState } = this.state
+    const { contentState } = this.state
+    console.log(contentState)
 
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">React Drafter</h1>
         </header>
-        <div className="editor">
-          <Editor
-            editorState={ editorState }
-            toolbarClassName="toolbar-editor"
-            wrapperClassName="wrapper-editor"
-            editorClassName="editor-editor"
-            onEditorStateChange={this.onEditorStateChange}
-          />
-        </div>
+        <form
+          onSubmit={ (event) => {
+            event.preventDefault()
+          }}
+        >
+          <div className="editor">
+            <Editor
+              onContentStateChange={this.onContentStateChange}
+            />
+          </div>
+        </form>
+        <textarea
+          disabled
+          value={contentState}/>
       </div>
     );
   }
---
+++ web/src/api/announcements.js
@@ -0,0 +1,9 @@
+export function listAnnouncements() {
+  return api.get('/announcements')
+    .then((res) => res.data)
+}
+
+export function createAnnouncement(data) {
+  return api.post('/announcements', data)
+    .then((res) => res.data)
+}
