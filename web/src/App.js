05b53a56917125e86ad72598100e3a1be0d5d3a6
Alex-Farries <adfarries@gmail.com>
2018-01-11 12:00:54 +1100

    Add basic wysiwyg editor functionality

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Editor } from 'react-draft-wysiwyg'
+import { EditorState } from 'draft-js'
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 
 class App extends Component {
+  state = {
+    editorState: EditorState.createEmpty()
+  }
 
-  
+  onEditorStateChange = (editorState) => {
+    this.setState({ editorState })
+  }
+
   render() {
+    const { editorState } = this.state
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">React CKEditor</h1>
+          <h1 className="App-title">React Drafter</h1>
         </header>
+        <div className="editor">
+          <Editor
+            editorState={ editorState }
+            toolbarClassName="toolbar-editor"
+            wrapperClassName="wrapper-editor"
+            editorClassName="editor-editor"
+            onEditorStateChange={this.onEditorStateChange}
+          />
+        </div>
       </div>
     );
   }
