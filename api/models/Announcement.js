61d2481af84ebfe31ab8d450e6ba3d8a391900d5
Alex-Farries <adfarries@gmail.com>
2018-01-12 19:06:50 +1100

    Update announcement model to have timestamps

--- api/models/Announcement.js
+++ api/models/Announcement.js
@@ -1,8 +1,13 @@
 const mongoose = require('./init')
 
-const Announcement = mongoose.model('Announcement', {
+const announcementSchema = new mongoose.Schema({
     subject: String,
-    content: {any: {} }, // "anything goes", could be better to be explicit about what we want from contentState
+    contentData: String,
+},
+{
+    timestamps: true
 })
+
+const Announcement = mongoose.model('Announcement', announcementSchema)
 
 module.exports = Announcement
